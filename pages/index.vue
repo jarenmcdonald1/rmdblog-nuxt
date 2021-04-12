732f2f13761e6fb26bb7890397015d582a430d6f
Jaren M <jaren.mcd@gmail.com>
2021-04-12 09:56:20 -0400

    updated with identity widget

--- pages/index.vue
+++ pages/index.vue
@@ -11,3 +11,13 @@
     </section>
   </main>
 </template>
+
+<script>
+export default {
+  head() {
+    return {
+      script: [{ src: 'https://identity.netlify.com/v1/netlify-identity-widget.js' }],
+    };
+  },
+};
+</script>
